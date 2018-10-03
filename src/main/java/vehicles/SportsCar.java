f67e0acae1a8bf6b03eebea8de81b9369b79be86
Pim <pimgroeneveld92@gmail.com>
2018-10-03 14:13:03 +0100

    adds components to sportsCar, electicCar still to go

--- src/main/java/vehicles/SportsCar.java
+++ src/main/java/vehicles/SportsCar.java
@@ -1,14 +1,36 @@
 package vehicles;
 
 import behaviours.Drivable;
+import components.Chassis;
+import components.Engine;
+import components.Wheels;
 
 public class SportsCar extends Vehicle implements Drivable {
 
-    public SportsCar(String model, int price, String colour, int maxSpeed) {
+    Engine engine;
+    Wheels wheels;
+    Chassis chassis;
+
+    public SportsCar(String model, int price, String colour, int maxSpeed, Engine engine, Wheels wheels, Chassis chassis) {
         super(model, price, colour, maxSpeed);
+        this.engine = engine;
+        this.wheels = wheels;
+        this.chassis = chassis;
     }
 
     public String drive(){
         return "Vroom Vroom! Super fast";
+    }
+
+    public String getEngine() {
+        return engine.getEngineType();
+    }
+
+    public String getWheels() {
+        return wheels.getType();
+    }
+
+    public String getChassis() {
+        return chassis.getMaterial();
     }
 }
--- src/test/java/vehicles/SportsCarTest.java
+++ src/test/java/vehicles/SportsCarTest.java
@@ -1,4 +1,7 @@
 package vehicles;
+import components.Engine;
+import components.Wheels;
+import components.Chassis;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +11,16 @@ import static org.junit.Assert.*;
 public class SportsCarTest {
 
     SportsCar sportsCar;
+    Engine engine;
+    Wheels wheels;
+    Chassis chassis;
 
     @Before
     public void before(){
-        sportsCar = new SportsCar("Ferrari", 60000, "blue", 280);
+        engine = new Engine("Gasoline");
+        wheels = new Wheels("Sport");
+        chassis = new Chassis("Carbon steel");
+        sportsCar = new SportsCar("Ferrari", 60000, "blue", 280, engine, wheels, chassis);
     }
 
     @Test
@@ -39,4 +48,18 @@ public class SportsCarTest {
         assertEquals("Vroom Vroom! Super fast", sportsCar.drive());
     }
 
+    @Test
+    public void getEngineType() {
+        assertEquals("Gasoline", sportsCar.getEngine());
+    }
+
+    @Test
+    public void getWheelType() {
+        assertEquals("Sport", sportsCar.getWheels());
+    }
+
+    @Test
+    public void getChassisMaterial() {
+        assertEquals("Carbon steel", sportsCar.getChassis());
+    }
 }
